da4f0529f71ff09b9dcfa4c1022acc72e9bd4617
Anthonalex <Anthonalex@users.noreply.github.com>
2021-03-04 00:34:40 +0400

    Add some examples of usage for both files

--- getRandomLetter.js
+++ getRandomLetter.js
@@ -1,3 +1,5 @@
 function getRandomLetter() {
-  return Math.random().toString(36).substring(2, 15);
+  return Math.random();
 }
+
+console.log(getRandomLetter());
--- getRandomNumber.js
+++ getRandomNumber.js
@@ -1,3 +1,5 @@
 function getRandomNumber() {
-  return Math.random();
+  return Math.random().toString(36).substring(2, 15);
 }
+
+console.log(getRandomNumber());
